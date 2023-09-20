78445342dc54c9ab7741eb59f77d9d63512098b9
Giovani Nardi <gionardi@hotmail.com>
2023-09-20 16:21:53 -0300

    Starting code release.

---
+++ MarsRover/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+namespace MarsRover
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Rover rover = new Rover();
+            Console.WriteLine(rover.Execute("MMMMMMMMMM"));
+            Console.WriteLine(rover.Execute("RMLM"));
+            Console.WriteLine(rover.Execute("MMMMMMMMM"));
+            Console.WriteLine(rover.Execute("RMLM"));
+        }
+    }
+}
+
+public interface IRover
+{
+    public string Execute(string command);
+}
+public class Rover: IRover
+{
+    int xPosition;
+    int yPosition;
+    int direction;
+    char[] directions = { 'N', 'E', 'S', 'W' };
+
+    public Rover()
+    {
+        Reset();
+    }
+
+    private void Reset()
+    {
+        xPosition = 0;
+        yPosition = 0;
+        direction = 0;
+    }
+    public string Execute(string command)
+    {        
+        string position = "";
+
+        foreach (char c in command) {
+
+            if (c == 'R')
+                direction = direction == 3 ? 0 : ++direction;
+
+            if (c == 'L')
+                direction = direction == 0 ? 3 : --direction;
+
+            if (c == 'M')
+            {
+                xPosition += direction == 1 ? 1 : direction == 3 ? -1 : 0;
+                yPosition += direction == 0 ? 1 : direction == 2 ? -1 : 0;
+            }
+
+            if (xPosition >= 10 || xPosition < 0 || yPosition >= 10 || yPosition < 0)
+                Reset();
+
+            position = xPosition + ":" + yPosition + ":" + directions[direction];            
+        }
+        return position;
+    }
+}
---
+++ TestMarsRover/UnitTest1.cs
@@ -0,0 +1,26 @@
+namespace TestMarsRover
+{
+    public class Tests
+    {
+        Rover rover = new Rover();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.That(rover.Execute("MMMMMMMMMM"), Is.EqualTo("0:0:N"));
+            Assert.That(rover.Execute("R"), Is.EqualTo("0:0:E"));
+            Assert.That(rover.Execute("M"), Is.EqualTo("1:0:E"));
+            Assert.That(rover.Execute("L"), Is.EqualTo("1:0:N"));
+            Assert.That(rover.Execute("M"), Is.EqualTo("1:1:N"));
+            Assert.That(rover.Execute("MM"), Is.EqualTo("1:3:N"));
+            Assert.That(rover.Execute("RM"), Is.EqualTo("2:3:E"));
+            Assert.That(rover.Execute("MMMMMMMM"), Is.EqualTo("0:0:N"));
+            Assert.Pass();
+        }
+    }
+}
